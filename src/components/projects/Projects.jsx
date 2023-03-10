444f1a4af495c3df751890931210d1144935cc92
lukmik <lukaszmlukaszewski@gmail.com>
2023-03-10 12:33:20 +0100

    project new ui

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,31 +4,50 @@ import chess from './board.png'
 import clock from './clock.png'
 import gomoku from './gomoku.png'
 import maze from './maze.png'
-import { HashLink as Link } from 'react-router-hash-link';
+import {MdKeyboardArrowRight} from 'react-icons/md'
 
 function Pro(props) {
   return (
     <div className="pro">
-      <div className="name">{props.name}</div>
-      <img src={props.img} alt="project"></img>   
-      <div className="links">
-        <div className="demo_link"><Link to={props.link}>Demo</Link></div>
-        <div className="code_link"><a href={props.code} target="_blank" rel="noreferrer">Code</a></div>
+      <img src={props.img} alt="project"></img>
+      <div className="text">
+        <div className="texttop">
+          <div className="name">{props.name}</div>    
+          <div className="description">{props.desc}</div>   
+        </div>
+        <div className="textbottom">
+          <div className="languages">{props.lang}</div>     
+          <div className="links">
+            <div className="demo_link"><a href={props.link} target="_blank" rel="noreferrer">Demo <div className="icon"><MdKeyboardArrowRight/></div></a></div>
+            <div className="code_link"><a href={props.code} target="_blank" rel="noreferrer"><div className="code">Code</div><div className="icon"><MdKeyboardArrowRight/></div></a></div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 const Projects = () => {
+  const lang_maze = ["Python", "Pygame", "PygBag"]
+  const maze_items = lang_maze.map((numbers) =>
+    <div className="lang">{numbers}</div>
+  );
+  const lang_clock = ["Python", "Tkinter"]
+  const clock_items = lang_clock.map((numbers) =>
+    <div className="lang">{numbers}</div>
+  );
+  const lang_gomoku = ["C++"]
+  const gomoku_items = lang_gomoku.map((numbers) =>
+    <div className="lang">{numbers}</div>
+  );
   return (
       <section id='projects'>      
         <div className="container projects__container">
-          <p className="titles">PROJECTS</p>
+          <p className="titles">Projects</p>
           <div className="projects">
-            <Pro link="/chess" img={chess} name="Chess" code="https://github.com/lukmikas"/>
-            <Pro link="/gomoku" img={gomoku} name="Gomoku" code="https://github.com/lukmikas/gomoku"/>
-            <Pro link="/clock" img={clock} name="Clock" code="https://github.com/lukmikas/clock"/>
-            <Pro link="/maze" img={maze} name="Maze" code="https://github.com/lukmikas/maze"/>
+            <Pro img={maze} name="Maze" desc="Program generates maze using prims algorithm and can find all paths using backtracking algorithm and shortest path using A* or one of two versions of Dijkstra algorithm" lang={maze_items} link="https://lukmikas.github.io/maze/"  code="https://github.com/lukmikas/maze"/>
+            <Pro img={clock} name="Clock" desc="Clock app that besides current time let's user set an alarm and choose a sound and a message. The app also allows user to set a timer and a stopwatch. It also offers light and dark mode." lang={clock_items} link="/clock"  code="https://github.com/lukmikas/clock"/>
+            <Pro img={gomoku} name="Gomoku" desc="Gomoku, also known as Five in a Row, is a two-player board game similar to tic-tac-toe but played on a 15x15 grid. The objective of the game is to be the first player to place five stones in a row horizontally, vertically, or diagonally." lang={gomoku_items} link="/gomoku" target="_blank" code="https://github.com/lukmikas/gomoku"/>
           </div>
         </div>
       </section>
--- src/components/projects/proj/Maze.jsx
+++ src/components/projects/proj/Maze.jsx
@@ -3,7 +3,8 @@ import './maze.css'
 const Maze = () => {
   return (
     <div className="container maze__container">
-        <div className="soon">COMING SOON</div>
+        {/* <div className="soon">COMING SOON</div> */}
+        <iframe frameborder="0" src="https://itch.io/embed-upload/7483535?color=333333" allowfullscreen="" width="640" height="380"><a href="https://lukmik.itch.io/maze">Play Maze Generator and Solver on itch.io</a></iframe>
     </div>
   )
 }
