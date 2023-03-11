f6bebb46d2fa4d24a481d9b5a27e130eb63e7a66
lukmik <lukaszmlukaszewski@gmail.com>
2023-03-11 22:15:42 +0100

    navbar hide on scroll

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Projects from './components/projects/Projects'
 import Nav from './components/nav/Nav'
 import Skills from './components/skills/Skills'
 import Contact from './components/contact/Contact'
-import Chess from './components/projects/proj/Chess'
 import Gomoku from './components/projects/proj/Gomoku'
 import Clock from './components/projects/proj/Clock'
 import Maze from './components/projects/proj/Maze'
@@ -15,10 +14,9 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Nav/>
-        <ScrollSpy scrollThrottle={100} useBoxMethod={false} offsetTop={100} offsetBottom={100}>
+        <ScrollSpy scrollThrottle={100} useBoxMethod={false} offsetTop={200} offsetBottom={100}>
           <Routes>
             <Route exact path="/" element={<><About/><Projects/><Skills/><Contact/></>}/>
-            <Route exact path="/chess" element={<><Chess/></>}/>  
             <Route exact path="/gomoku" element={<><Gomoku/></>}/>  
             <Route exact path="/clock" element={<><Clock/></>}/>     
             <Route exact path="/maze" element={<><Maze/></>}/>  
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React, {useState, useRef, useEffect} from "react"
+import useScrollListener from "./hooks/useScrollListener";
 import "./nav.css"
 import { Link } from "react-scroll";
 
 const Nav = () => {
-  const [mobileNav, setMobileNav] = useState(false);
+  const [navClassList, setNavClassList] = useState([]);
+  const scroll = useScrollListener();
+
+  useEffect(() => {
+    const _classList = [];
+
+    if (scroll.y > 50 && scroll.y - scroll.lastY > 0)
+      _classList.push("navbar_hidden");
+    setNavClassList(_classList);
+  }, [scroll.y, scroll.lastY]);
+
   const navRef = useRef(null);
   const [navOffset, setNavOffset] = useState();
   const getHeight= () => {
@@ -18,39 +29,21 @@ const Nav = () => {
     window.addEventListener("resize", getHeight);
   }, []);
   return (
-    <div ref={navRef} className="navbar" onClick={() => {setMobileNav(!mobileNav);}}>   
+    <nav ref={navRef} className={navClassList.join("")}>   
       <div  class="container nav__container">
         <div class="name">
           <a href="/portfolio">
             Łukasz Łukaszewski
           </a>
         </div>  
-        <div className="links">
-        <Link to='about' className="A" activeClass="active" data-to-scrollspy-id="about" smooth={true} duration={500} isDynamic={true} offset={-navOffset}>About</Link>
-        <Link to='projects' className="A" activeClass="active" data-to-scrollspy-id="projects"  smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Projects</Link>
-        <Link to='skills' className="A" activeClass="active" data-to-scrollspy-id="skills"   smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Skills</Link>
-        <Link to='contact' className="A" activeClass="active" data-to-scrollspy-id="contact"  smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Contact</Link>
-        </div>
-        {/* <button className="hamburger" onClick={() => {setMobileNav(!mobileNav);}}>
-        <div className="icon"><GiHamburgerMenu size={30} /></div></button>
-        <div className={mobileNav ? "mobile_links" : "links"}>
-          {mobileNav ? (    
-            <>     
-            <Link to='/#about' className="A" data-to-scrollspy-id="about" onClick={() =>            {setMobileNav(!mobileNav);}}>About</Link>
-            <Link to='/#projects' className="A" data-to-scrollspy-id="projects" onClick={() =>            {setMobileNav(!mobileNav);}}>Projects</Link>
-            <Link to='/#skills' className="A" data-to-scrollspy-id="skills" onClick={() =>            {setMobileNav(!mobileNav);}}>Skills</Link>
-            <Link to='/#contact' className="A" data-to-scrollspy-id="contact" onClick={() =>            {setMobileNav(!mobileNav);}}>Contact</Link>
-            </> 
-            ): (
-            <>
-            <Link to='about' className="A" activeClass="active" data-to-scrollspy-id="about"   smooth={true} duration={500} isDynamic={true} offset={-navOffset}>About</Link>
-            <Link to='projects' className="A" activeClass="active" data-to-scrollspy-id="projects"  smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Projects</Link>
-            <Link to='skills' className="A" activeClass="active" data-to-scrollspy-id="skills"  smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Skills</Link>
+          <div className="links">
+            <Link to='about' className="A" activeClass="active" data-to-scrollspy-id="about" smooth={true} duration={500} isDynamic={true} offset={-navOffset}>About</Link>
+            <Link to='projects' className="A" activeClass="active" data-to-scrollspy-id="projects" smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Projects</Link>
+            <Link to='skills' className="A" activeClass="active" data-to-scrollspy-id="skills"   smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Skills</Link>
             <Link to='contact' className="A" activeClass="active" data-to-scrollspy-id="contact"  smooth={true} duration={500} isDynamic={true} offset={-navOffset}>Contact</Link>
-            </>
-          )} */}    
+          </div>    
       </div>
-    </div>
+    </nav>
   );
 };
 
